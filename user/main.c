073bfb66a8ec1c214bd3d0186baf84bc75ee0a81
syd <yaodongshi@foxmail.com>
2023-09-08 06:55:36 +0800

    CLion 中 GD32F4xx 开发模版

---
+++ user/main.c
@@ -0,0 +1,25 @@
+#include "gd32f4xx.h"
+#include "stdio.h"
+#include "delay.h"
+//#include "main.h"
+#include "led.h"
+#include "usart.h"
+int main(void)
+{
+        delay_init(200);
+        LED_Init();
+        USART_Init(115200);
+
+        unsigned int temp = 0;
+        while (1) {
+                printf("%s %d\n", "test", temp++);
+                LED_ON;
+                USART_Send_String((uint8_t *) "off\n");
+                //delay_1ms(500);
+                delay_ms(1000);
+                LED_OFF;
+                USART_Send_String((uint8_t *) "no\n");
+                //delay_1ms(500);
+                delay_ms(1000);
+        }
+}
